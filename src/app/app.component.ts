1219d7f6a989044b5f271d30102dc50e65b50f0c
Maak <realdjmaku@gmail.com>
2021-03-22 10:07:52 +0200

    Added angular build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, XhrFactory } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'konesyntees-app';
+
+  url = 'https://neurokone.cloud.ut.ee/api/v1.0/synthesize'; 
+
+  constructor(private http: HttpClient) {
+    }  
+
+    listenText(item: string) {
+      console.log(item);
+      let postData = {
+        'text': item,
+        'speaker_id': 1
+      }
+
+      this.http.post(this.url, postData, {responseType:'blob'}).toPromise().then(
+        data => {
+          let temp = window.webkitURL.createObjectURL(data);
+          let audio = new Audio(temp);
+          audio.play();
+        })
+    }
+
+    saveText(item: string) {
+      console.log(item);
+      let postData = {
+        'text': item,
+        'speaker_id': 1
+      }
+
+      this.http.post(this.url, postData, {responseType:'blob'}).toPromise().then(
+        data => {
+          let temp = window.webkitURL.createObjectURL(data);
+          let a = document.createElement('a');
+          a.setAttribute('hidden','');
+          a.setAttribute('href',temp);
+          a.setAttribute('download','synteesitud_tekst');
+          document.body.appendChild(a);
+          a.click();
+          document.body.removeChild(a);
+        })
+    }
+    
 }
